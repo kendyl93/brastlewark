74956cae1a274de7570a0c7ae3d1e443083e85f9
Pawel Stanecki <pawel.stanecki@sandstream.pl>
2019-08-19 22:45:27 +0200

    build

--- precache-manifest.a2d151d0d46c5f1ab1f159f6d361ba5b.js
+++ precache-manifest.a2d151d0d46c5f1ab1f159f6d361ba5b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e45e86f5a485d9f929a9",
+    "revision": "c2526390ac355c7ca352",
     "url": "./static/css/main.c5f364b7.chunk.css"
   },
   {
-    "revision": "e45e86f5a485d9f929a9",
-    "url": "./static/js/main.598ad2c8.chunk.js"
+    "revision": "c2526390ac355c7ca352",
+    "url": "./static/js/main.75aaac55.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "./static/media/error.39bd5956.png"
   },
   {
-    "revision": "d10e6ed3fcb168a1b69adefef3677344",
+    "revision": "5b6aeaedc70e4f39db87d43e4c610187",
     "url": "./index.html"
   }
 ];
